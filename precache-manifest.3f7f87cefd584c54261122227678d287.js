5d1ea3ee256e70e274dba3dfcb77cef6a1be0d1d
Potapov Aleksey <alex.potap1811@gmail.com>
2019-06-16 15:43:19 +0300

    Updates

---
+++ precache-manifest.3f7f87cefd584c54261122227678d287.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b63c1f6e34493422d449d2392690d2b",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "358a89b20f1de118f0e9",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.c4887395.chunk.js"
+  },
+  {
+    "revision": "a28a6d1b9ae16db40c54",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.c5d23f5f.chunk.js"
+  },
+  {
+    "revision": "549cb78f4abd9d607011",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime~main.0cda3cff.js"
+  }
+]);
