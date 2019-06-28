79e52c2fe473b08c15884d6bc1c12b3de6be8ad9
Potapov Aleksey <alex.potap1811@gmail.com>
2019-06-28 15:31:20 +0300

    Updates

---
+++ precache-manifest.4459f9d233962b6d0fcef76ee670eec0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac4bf58c0f30fefe0667f8759a59ab12",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "885f483a56b82ba0751e",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.0ff8362f.chunk.css"
+  },
+  {
+    "revision": "77bd09398a72123ee683",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.68f1f250.chunk.js"
+  },
+  {
+    "revision": "885f483a56b82ba0751e",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.3a3bc096.chunk.js"
+  },
+  {
+    "revision": "549cb78f4abd9d607011",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime~main.0cda3cff.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-05-budget-planner/precache-manifest.29540d5cb0e95324dcced0aebd56bdad.js"
+  "/goit-react-hw-05-budget-planner/precache-manifest.4459f9d233962b6d0fcef76ee670eec0.js"
 );
 
 self.addEventListener('message', (event) => {
